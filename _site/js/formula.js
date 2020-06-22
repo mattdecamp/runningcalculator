cab3bdf75ba2ded70f8716c69b1e470608e12f66
Matthew DeCamp <matthewdecamp@matthews-mbp.lan>
2020-06-22 16:37:46 -0400

    formula adjustments

--- _site/js/formula.js
+++ _site/js/formula.js
@@ -7,42 +7,32 @@ const pacePerMile = function () {
     // console.log(hours + " " + minutes + " " + seconds + " " + mileage);
     // This combines the seconds, minutes and hours into total seconds and divides that by mileage to get seconds per mile
     const secondsPerMile = (seconds + minutes * 60 + hours * 60 * 60) / mileage;
-    console.log("seconds per mile:" + " " + secondsPerMile);
     // Calculates the number of hours per mile;
     const paceHours = Math.floor(secondsPerMile / 3600);
-    console.log("Hours:" + " " + paceHours);
     const timeLeft = secondsPerMile - paceHours * 3600;
-    console.log("Time Left:" + " " + timeLeft);
     // Converts secondsPerMile to a minutes per mile in decimal form
     const minutesDecimal = timeLeft / 60;
     // From minutesDecimal assigns everything left of the decimal point to left
     const paceMinutes = Math.floor(minutesDecimal);
-    console.log("pace in minutes:" + " " + paceMinutes);
     // From minutesDecimal assigns everything right of the decimal point to rightDecimal
     const paceSeconds = Math.round((minutesDecimal % 1) * 60);
-    console.log("Seconds" + " " + paceSeconds);
     // Adds zeros to each field if needed
     function zeroes(string, pad, length) {
         return (new Array(length + 1).join(pad) + string).slice(-length);
     }
     const pace =
-        zeroes(paceHours, "0", 2) +
-        ":" +
-        zeroes(paceMinutes, "0", 2) +
-        ":" +
-        zeroes(paceSeconds, "0", 2);
-    // Prints pace per mile to the console
-    console.log(hours);
-    console.log(minutes);
-    console.log(seconds);
-    console.log(mileage);
-    console.log(pace);
+    zeroes(paceHours, "0", 2) +
+    ":" +
+    zeroes(paceMinutes, "0", 2) +
+    ":" +
+    zeroes(paceSeconds, "0", 2);
     document.runCalculator.result.value = pace;
+    console.log(hours);
 };
 
 // // This combines the seconds, minutes and hours into TOTAL SECONDS
 // const timeSeconds = function timeInSeconds(seconds, minutes, hours) {
-//     return seconds + minutes * 60 + hours * 60 * 60;
+    //     return seconds + minutes * 60 + hours * 60 * 60;
 // };
 
 // // Takes total seconds and computes seconds per mile
