b7314bb1833b664d8ce8cd39007d565cbc607fef
Matthew DeCamp <matthewdecamp@matthews-mbp.lan>
2020-06-29 19:26:32 -0400

    Distance measurement added; style updates

--- _site/js/formula.js
+++ _site/js/formula.js
@@ -1,10 +1,25 @@
+function distanceSystem() {
+    const measurement = document.querySelector("#distanceMeasure");
+    // Changes pace per value based on distance select field
+    measurement.addEventListener('change', (e) => {
+        const paceMeasure = document.querySelector("#paceMeasure");
+        if (e.target.value === "miles") {
+            paceMeasure.textContent = "/mi";
+        } else if (e.target.value === "kilometers") {
+            paceMeasure.textContent = "/km";
+        } else {
+            paceMeasure.textContent = "/yd";
+        }
+    });
+}
+distanceSystem();
+
 // This is the main calculating function for Pace Per Mile
 const pacePerMile = function () {
     const hours = parseInt(document.runCalculator.hours.value);
     const minutes = parseInt(document.runCalculator.minutes.value);
     const seconds = parseInt(document.runCalculator.seconds.value);
     const mileage = parseFloat(document.runCalculator.mileage.value); // parseFloat ensures decimal input option
-    // console.log(hours + " " + minutes + " " + seconds + " " + mileage);
     // This combines the seconds, minutes and hours into total seconds and divides that by mileage to get seconds per mile
     const secondsPerMile = (seconds + minutes * 60 + hours * 60 * 60) / mileage;
     // Calculates the number of hours per mile;
@@ -16,6 +31,9 @@ const pacePerMile = function () {
     const paceMinutes = Math.floor(minutesDecimal);
     // From minutesDecimal assigns everything right of the decimal point to rightDecimal
     const paceSeconds = Math.round((minutesDecimal % 1) * 60);
+    // Calculate based on distance measurement
+
+
     // Adds zeros to each field if needed
     function zeroes(string, pad, length) {
         return (new Array(length + 1).join(pad) + string).slice(-length);
@@ -26,6 +44,7 @@ const pacePerMile = function () {
     zeroes(paceMinutes, "0", 2) +
     ":" +
         zeroes(paceSeconds, "0", 2);
+    // Replace error output with zeros
     if (pace === "aN:aN:aN") {
         document.runCalculator.result.value = "00:00:00";
     } else {
@@ -33,39 +52,4 @@ const pacePerMile = function () {
     }
 };
 
-// // This combines the seconds, minutes and hours into TOTAL SECONDS
-// const timeSeconds = function timeInSeconds(seconds, minutes, hours) {
-    //     return seconds + minutes * 60 + hours * 60 * 60;
-// };
 
-// // Takes total seconds and computes seconds per mile
-// const secondsPerMile = function secondsMiles(timeSeconds, mileage) {
-//     return timeSeconds / mileage;
-// };
-
-// // Converts secondsPerMile to a minutes per mile in decimal form
-// const minutesDecimal = function minutesAsDecimal(secondsPerMile) {
-//     return secondsPerMile / 60;
-// };
-
-// // From minutesDecimal assigns everything right of the decimal point to rightDecimal
-// const rightDecimal = function calcRightDecimal(minuteDecimal) {
-//     return minuteDecimal % 1;
-// };
-
-// // From minutesDecimal assigns everything left of the decimal point to leftDecimal
-// const leftDecimal = function calcLeftDecimal(minuteDecimal) {
-//     return Math.floor(minuteDecimal);
-// };
-
-// // Converts rightDecimal to seconds
-// const paceSeconds = function (rightDecimal) {
-//     return rightDecimal * 60;
-// };
-
-// // Re-assigns leftDecimal to paceMinutes
-// const paceMinutes = function (leftDecimal) {
-//     return leftDecimal;
-// };
-
-//!!! Need to include hours as well!
